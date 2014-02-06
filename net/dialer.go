5e4107ce6f20ad08a085c4e05d4b7e40498c4db4
Sajal Kayan <sajal@turbobytes.com>
2014-02-06 21:39:28 +0700

    take timeout as argument

--- net/dialer.go
+++ net/dialer.go
@@ -8,30 +8,32 @@ import (
 	"time"
 )
 
-type Dialresponse struct {
+type dialresponse struct {
 	Conn *net.TCPConn
 	Err  error
 }
 
-func dialer(netw string, srcaddr, tcpaddr *net.TCPAddr, c chan Dialresponse) {
+func dialer(netw string, srcaddr, tcpaddr *net.TCPAddr, c chan dialresponse) {
 	conn, err := net.DialTCP(netw, srcaddr, tcpaddr)
-	response := Dialresponse{Conn: conn, Err: err}
+	response := dialresponse{Conn: conn, Err: err}
 	c <- response
 	//return c
 }
 
-func DialTCPTimeout(netw string, laddr, raddr *net.TCPAddr) (*net.TCPConn, error) {
+// net.DialTCP but accept a timeout net.DialTimeout doesnt take laddr..
+// net.DialTCP doesnt take a timeout
+func DialTCPTimeout(netw string, laddr, raddr *net.TCPAddr, timeout time.Duration) (*net.TCPConn, error) {
 	//Do this hanky panky cause there is no DialTCPTimeout in net
 	var conn *net.TCPConn
 	var err error
-	c := make(chan Dialresponse)
+	c := make(chan dialresponse)
 	go dialer(netw, laddr, raddr, c)
 	select {
 	case response := <-c:
 		conn = response.Conn
 		err = response.Err
 		//return conn, err
-	case <-time.After(20 * time.Second):
+	case <-time.After(timeout):
 		err = fmt.Errorf("FAIL conn from %s to %s took too long", laddr, raddr)
 
 	}
